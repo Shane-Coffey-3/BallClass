2ccd63bcbbd17e4d6b015ef6255deaa6b332678b
Shane-Coffey-3 <shanejcoffey27@gmail.com>
2025-01-28 13:50:03 -0600

    Random Angle Works

--- src/Arena.java
+++ src/Arena.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Arena extends JPanel {
     public static final boolean RANDOM_ANGLE = true;
     private ArrayList<Ball> balls = new ArrayList<>();
-
+    
     public Arena(int numBalls){
         setBackground(Color.blue);
         for(int i = 0; i < numBalls; i++) {
@@ -16,11 +16,11 @@ public class Arena extends JPanel {
             double randomYSpeed = Math.random() * 4 - 2.9;
             int randomSize = (int) (Math.random() * 40) + 10;
             Color randomColor = new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
-
+            
             balls.add(new Ball(randomX, randomY, randomXSpeed, randomYSpeed, randomSize, randomColor));
         }
     }
-
+    
     @Override
     public void paintComponent(Graphics g) {
         for (Ball ball : balls) {
@@ -32,12 +32,12 @@ public class Arena extends JPanel {
                 ball.bounce(this.getWidth(), this.getHeight());
             }
         }
-
+        
         try {
-            Thread.sleep(10);
+            Thread.sleep(4);
         } catch(Exception e){}
-
-
+        
+        
         repaint();
     }
 }
